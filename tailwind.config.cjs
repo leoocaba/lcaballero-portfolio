8055ba304b73b42aa1e9401303b8f3a5a8fa0b58
leoocaba <leoo.caballero@gmail.com>
2024-12-30 01:10:06 -0300

    Ajustes de color principal, padding y margin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
     extend: {
       backgroundImage: {
         'custom-radial': 'radial-gradient(125% 125% at 50% 10%, #000 40%, #16a34a 100%)',
-      }
+      },
+      scrollBehavior: ['smooth'],
+      colors: {
+        customGreen: '#16a34a',
+      },
     },
   },
   plugins: [require('daisyui')],
