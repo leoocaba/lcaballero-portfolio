cc49ad87325a7078e357fcf1c8333c71f10f213f
leoocaba <leoo.caballero@gmail.com>
2026-02-24 12:18:37 -0300

    Modificaciones para deploy en server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://leoocaba.github.io/lcaballero-portfolio',
-    base: '/lcaballero-portfolio/',
+    site: 'https://leonelcaballero.com',
     output: 'static',
   });
