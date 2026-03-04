7f602008d6b6e95ff2d0cb25316b56073a09bffb
leoocaba <leoo.caballero@gmail.com>
2026-03-04 07:04:13 -0300

    Se aagregó sitemap y robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://leonelcaballero.com',
-    output: 'static',
-  });
+  site: 'https://leonelcaballero.com',
+  output: 'static'
+});
+
